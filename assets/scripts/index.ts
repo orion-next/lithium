29b50f500833bd7743b1125c8a51af4086ca3f63
Vidush H. Namah <vidush.namah@orion-next.com>
2024-08-02 10:15:28 +0400

    Remove force-dark mode - light mode is stable enough

--- assets/scripts/index.ts
+++ assets/scripts/index.ts
@@ -7,7 +7,7 @@ const Application = {
         const COLOR_SCHEME_KEY = `${window.location.hostname}-ColorScheme`;
 
         // Force dark mode during beta phase
-        localStorage.setItem(COLOR_SCHEME_KEY, "dark");
+        // localStorage.setItem(COLOR_SCHEME_KEY, "dark");
 
         const savedScheme = localStorage.getItem(COLOR_SCHEME_KEY);
         const colorScheme = savedScheme ?? document.documentElement.dataset.defaultTheme ?? "auto";
